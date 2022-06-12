77fab8a256a4a5c7cc97e08fc6643c46cde5cf17
atharizaki <atharizaki4@gmail.com>
2022-06-12 21:46:32 +0700

    Todo App Firebase PAM

---
+++ app/src/main/java/com/example/todoappfirebase/user.java
@@ -0,0 +1,40 @@
+package com.example.todoappfirebase;
+
+import com.google.firebase.firestore.auth.User;
+
+public class user {
+    private String id, name, email;
+
+    public user(){
+
+    }
+
+    public user(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+}
